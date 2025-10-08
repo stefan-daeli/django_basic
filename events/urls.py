9365216819c12ba9d1b63d57661c0ad445868676
stefan-daeli <stefanusdaeli2003@gmail.com>
2025-10-08 14:47:38 +0700

    first commit

---
+++ events/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Path Converters
+    # int: numbers
+    # str: strings
+    # path: whole urls /
+    # slug: hyphen and underscore
+    # UUID: universally unique identifier
+
+    # path('', views.home, name='home'),
+    path('<int:year>/<str:month>/', views.home, name='home'),
+
+]
---
+++ events/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+import calendar
+from calendar import HTMLCalendar
+from datetime import datetime
+
+def home(request, year, month):
+    name = "John"
+    month = month.capitalize()
+    # convert month from name to number
+    month_number = list(calendar.month_name).index(month)
+    month_number = int(month_number)
+    # create a calendar
+    cal = HTMLCalendar().formatmonth(
+        year, 
+        month_number)
+    
+    # get current year
+    now = datetime.now()
+    current_year = now.year
+
+    # get current time
+    current_time = now.strftime('%I:%M:%S %p')
+    return render(request, 'home.html', 
+                  {'name' : name,
+                   'year' : year,
+                   'month' : month,
+                   'month_number' : month_number,
+                   'cal' : cal,
+                   'current_year' : current_year,
+                   'current_time' : current_time
+                   })

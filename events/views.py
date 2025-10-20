a7d8a27f476fd7baa20cbfad301fb0bb4e0e59be
stefan-daeli <stefanusdaeli2003@gmail.com>
2025-10-20 09:03:05 +0700

    session 12

--- events/views.py
+++ events/views.py
@@ -6,6 +6,17 @@ from django.http import HttpResponseRedirect
 from .models import Event, Venue
 from .forms import VenueForm, EventForm
 
+# Delete an Venue
+def delete_venue(request, venue_id):
+    venue = Venue.objects.get(pk=venue_id)
+    venue.delete()
+    return redirect('list-venues')
+# Delete an Event
+def delete_event(request, event_id):
+    event = Event.objects.get(pk=event_id)
+    event.delete()
+    return redirect('list-events')
+
 def update_event(request, event_id):
     event = Event.objects.get(pk=event_id)
     form = EventForm(request.POST or None, instance=event)

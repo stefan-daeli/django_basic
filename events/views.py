280b038ee8c414759ddd1acc8d0b496b1334f157
stefan-daeli <stefanusdaeli2003@gmail.com>
2025-10-21 08:30:02 +0700

    session 16

--- events/views.py
+++ events/views.py
@@ -7,6 +7,54 @@ from .models import Event, Venue
 from .forms import VenueForm, EventForm
 from django.http import HttpResponse
 import csv
+from django.http import FileResponse
+#io=input output
+import io
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import inch
+from reportlab.lib.pagesizes import letter
+
+# generate a PDF file venue list
+def venue_pdf(reuqest):
+    # create bytestream buffer
+    buf = io.BytesIO()
+    # create a canvas
+    c = canvas.Canvas(buf, pagesize=letter, bottomup=0)
+    # create a text object
+    textob = c.beginText()
+    textob.setTextOrigin(inch, inch)
+    textob.setFont("Helvetica", 14)
+
+    # add some lines of text
+    # lines = [
+    #     "This is line 1",
+    #     "This is line 2",
+    #     "This is line 3",
+    # ]
+    # designate the model
+    venues = Venue.objects.all()
+    lines = []
+    for venue in venues:
+        lines.append(venue.name)
+        lines.append(venue.address)
+        lines.append(venue.zip_code)
+        lines.append(venue.phone)
+        lines.append(venue.web)
+        lines.append(venue.email_address)
+        lines.append(" ")
+
+    # loop
+    for line in lines:
+        textob.textLine(line)
+
+    # finish up
+    c.drawText(textob)
+    c.showPage()
+    c.save()
+    buf.seek(0)
+
+    # return
+    return FileResponse(buf, as_attachment=True, filename='venues.pdf')
 
 def venue_csv(request):
     response = HttpResponse(content_type='text/csv') #use csv not plain
